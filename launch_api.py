b18a3c1e991e5f3d6dde6aa7a0f79cdf2d7646c4
Brian Breeden <bcbreeden@proton.me>
2024-01-15 16:56:11 -0500

    added logic to build a dev and prod api stub.

--- launch_api.py
+++ launch_api.py
@@ -1,13 +1,20 @@
 import requests
 
-API_STUB = 'https://ll.thespacedevs.com/2.2.0/launch/'
-
 def make_api_call(endpoint=''):
     '''
     Makes an api call to Launch Library 2. If not endpoint is provided, a call is made to the stub by default.
     '''
-    api_url = '{}{}'.format(API_STUB, endpoint)
+    api_url = '{}{}'.format(_get_api_stub(), endpoint)
     response = requests.get(api_url)
     json = response.json()
     status_code = response.status_code
     return[status_code, json]
+
+def _get_api_stub(dev=True):
+    '''
+    Builds the API stub. Defaults to the development instance of the API.
+    '''
+    stub = 'https://lldev.thespacedevs.com/2.2.0/launch/'
+    if not dev:
+        stub = 'https://ll.thespacedevs.com/2.2.0/launch/'
+    return stub
--- test_api.py
+++ test_api.py
@@ -1,7 +1,21 @@
 import unittest
-from launch_api import make_api_call
+from launch_api import make_api_call, _get_api_stub
 
-class TestAPI(unittest.TestCase):    
+class TestAPI(unittest.TestCase):
+    def test_dev_api_stub(self):
+        '''
+        Test the function that builds the API stub (dev).
+        '''
+        test_stub = _get_api_stub(dev=True)
+        self.assertEqual(test_stub, 'https://lldev.thespacedevs.com/2.2.0/launch/')
+
+    def test_prod_api_stub(self):
+        '''
+        Test the function that builds the API stub (prod).
+        '''
+        prod_stub = _get_api_stub(dev=False)
+        self.assertEqual(prod_stub, 'https://ll.thespacedevs.com/2.2.0/launch/')
+
     def test_api_default(self):
         '''
         Test the API call default (all launch objects).
