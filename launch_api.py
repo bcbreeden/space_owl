40a5b77934d5a869bbf30fca1d933aa8e03cf1ec
Brian Breeden <bcbreeden@proton.me>
2024-01-16 12:01:51 -0500

    Modified the api base and error handling

--- launch_api.py
+++ launch_api.py
@@ -3,23 +3,25 @@ import logging
 
 def make_api_call(endpoint=''):
     '''
-    Makes an api call to Launch Library 2. If not endpoint is provided, a call is made to the stub by default.
+    Makes an api call to Launch Library 2. If no endpoint is provided, a call is made to the stub by default.
     '''
-    api_url = '{}{}'.format(_get_api_stub(), endpoint)
+    api_url = '{}{}'.format(_get_api_base(), endpoint)
     response = requests.get(api_url)
     try:
         json = response.json()
-    except:
-        logging.warning('No JSON payload present: {}'.format(api_url))
+    except Exception as e:
+        logging.warning('No JSON payload present: {} - {}'.format(type(e).__name__, api_url))
+        logging.warning('Status Code: {}'.format(response.status_code))
+        logging.warning('Exception returned: {}'.format(e))
         json = None
     status_code = response.status_code
     return[status_code, json]
 
-def _get_api_stub(dev=True):
+def _get_api_base(dev=True):
     '''
-    Builds the API stub. Defaults to the development instance of the API.
+    Builds the API url base. Defaults to the development instance of the API.
     '''
-    stub = 'https://lldev.thespacedevs.com/2.2.0/launch/'
+    stub = 'https://lldev.thespacedevs.com/2.2.0/'
     if not dev:
-        stub = 'https://ll.thespacedevs.com/2.2.0/launch/'
+        stub = 'https://ll.thespacedevs.com/2.2.0/'
     return stub
--- test_api.py
+++ test_api.py
@@ -1,20 +1,20 @@
 import unittest
-from launch_api import make_api_call, _get_api_stub
+from launch_api import make_api_call, _get_api_base
 
 class TestAPI(unittest.TestCase):
     def test_dev_api_stub(self):
         '''
         Test the function that builds the API stub (dev).
         '''
-        test_stub = _get_api_stub(dev=True)
-        self.assertEqual(test_stub, 'https://lldev.thespacedevs.com/2.2.0/launch/')
+        test_base = _get_api_base(dev=True)
+        self.assertEqual(test_base, 'https://lldev.thespacedevs.com/2.2.0/')
 
     def test_prod_api_stub(self):
         '''
         Test the function that builds the API stub (prod).
         '''
-        prod_stub = _get_api_stub(dev=False)
-        self.assertEqual(prod_stub, 'https://ll.thespacedevs.com/2.2.0/launch/')
+        prod_base = _get_api_base(dev=False)
+        self.assertEqual(prod_base, 'https://ll.thespacedevs.com/2.2.0/')
 
     def test_api_default(self):
         '''
@@ -27,7 +27,7 @@ class TestAPI(unittest.TestCase):
         '''
         Test the API call for upcoming launches objects.
         '''
-        response = make_api_call('upcoming')
+        response = make_api_call('launch/upcoming')
         self.assertEqual(response[0], 200)
     
     def test_api_failure(self):
@@ -35,7 +35,7 @@ class TestAPI(unittest.TestCase):
         Test the API call when the endpoint does not exist.
         '''
         response = make_api_call('hooplah')
-        self.assertEqual(response[0], 500)
+        self.assertEqual(response[0], 404)
         self.assertEqual(response[1], None)
     
 
